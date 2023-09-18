5776c845ca40eaee692d5e021f4ebeaf3fc964aa
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-09-18 13:20:51 -0300

    started subprocess

--- subprocess/subprocess.cpp
+++ subprocess/subprocess.cpp
@@ -1,1 +1,27 @@
-#include <ostream>
+#include <iostream>
+
+namespace subprocess{
+    class Popen{
+
+    };
+
+    Popen run(){
+
+    }
+
+    Popen check_output(){
+
+    }
+
+    Popen call(){
+
+    }
+
+    Popen check_call(){
+
+    }
+}
+
+int main(){
+    return 0;
+}
